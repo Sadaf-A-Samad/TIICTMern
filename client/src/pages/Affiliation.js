669c60e92746c5ddf47c09d34495cf1cf4d35404
Sadaf-A-Samad <sadaf.shaikh2k3@gmail.com>
2024-07-14 22:34:27 +0500

    2nd Commit on 14-7-2014 - Routes Created with rough navbar

---
+++ client/src/pages/Affiliation.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Affiliation() {
+  return <div>Affiliation</div>;
+}
+
+export default Affiliation;
---
+++ client/src/pages/TiictBlog.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Post from "./Post";
+
+function TiictBlog() {
+  return (
+    <>
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+    </>
+  );
+}
+
+export default TiictBlog;
