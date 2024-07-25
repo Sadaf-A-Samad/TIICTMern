26143dcd60b1694545935396e26ac73d05368687
Sadaf-A-Samad <sadaf.shaikh2k3@gmail.com>
2024-07-25 10:09:01 +0500

    25-7-24 - 1st Draft - Complete Design

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -3,12 +3,19 @@ import React from "react";
 const Login = () => {
   return (
     <>
-      <form className="login">
-        <h1>Login</h1>
-        <input type="text" placeholder="User Name" />
-        <input type="password" placeholder="Password" />
-        <button>Login</button>
-      </form>
+      <section id="formReg" className="formReg bg2">
+        <div className="container">
+          <form className="login" data-aos="fade-up">
+            <div className="section-title">
+              {/* <h2></h2> */}
+              <p>Login Form</p>
+            </div>
+            <input type="text" placeholder="User Name" />
+            <input type="password" placeholder="Password" />
+            <button>Login</button>
+          </form>
+        </div>
+      </section>
     </>
   );
 };
--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -3,12 +3,19 @@ import React from "react";
 const Register = () => {
   return (
     <>
-      <form className="register">
-        <h1>Register</h1>
-        <input type="text" placeholder="User Name" />
-        <input type="password" placeholder="Password" />
-        <button>Register</button>
-      </form>
+      <section id="formReg" className="formReg bg2">
+        <div className="container">
+          <form className="register" data-aos="fade-up">
+            <div className="section-title">
+              {/* <h2>Contact</h2> */}
+              <p>Register</p>
+            </div>
+            <input type="text" placeholder="User Name" />
+            <input type="password" placeholder="Password" />
+            <button>Register</button>
+          </form>
+        </div>
+      </section>
     </>
   );
 };
